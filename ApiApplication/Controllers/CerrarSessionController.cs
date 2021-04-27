7ac5dbdb517f6fef50ce18acc1c5d409fd7703e8
Ranicaro <nicolascaceresrom@hotmail.com>
2021-04-26 20:17:08 -0500

    cerrar sesion

---
+++ ApiApplication/Controllers/CerrarSessionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Utilitarios;
+using Logica;
+namespace ApiApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class CerrarSessionController : ApiController
+    {
+        [HttpPost]
+        [Route("api/CerrarSession/PostPage_Load")]
+        public void Page_Load(UUsuario usuario1){
+            new LCerrarSession().Page_Load(usuario1);
+        }
+    }
+}

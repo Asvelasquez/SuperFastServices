a9b9e37b6ed1e00fb4b57ba7a1f11de0be9ce574
3RR0R404 <32886956+Asvelasquez@users.noreply.github.com>
2021-06-06 00:58:01 -0500

    domiciliario archivos

--- ApiApplication/Controllers/Registrar_DomiciliarioController.cs
+++ ApiApplication/Controllers/Registrar_DomiciliarioController.cs
@@ -21,30 +21,39 @@ namespace ApiApplication.Controllers{
         /// Este metodo Permite registrar un usuario tipo domiciliario
         /// </summary>
         /// <param name="Vs_entrada"></param>
-        [HttpGet]
-        [Route("api/Registrar_Domiciliario/GetRegitrar_domiciliario")]
-        public string Regitrar_domiciliario([FromBody] JObject Vs_entrada){
+        [HttpPost]
+        [Route("api/Registrar_Domiciliario/Registrar_domiciliario")]
+        public string Registrar_domiciliario([FromBody] JObject Vs_entrada){
             string correo;
             try{
+               
                 correo = Vs_entrada["correo"].ToString();
                 UUsuario validarUsuario = new LRegistrarse().LBT_Registrar(correo);
                 if (validarUsuario != null){
                     return "correo registrado,ingrese uno diferente";
                 }else{
                     UUsuario usuario = new UUsuario();
+                    String archivo = Vs_entrada["archivo"].ToString();
+
+                    byte[] hoja_vida = Convert.FromBase64String(archivo);
+                  
+                    string nombredocumento = Vs_entrada["nombre"].ToString();
+                    string extension = Vs_entrada["extension"].ToString();
+                    string direccion = "~\\Domiciliario\\HojaVida\\" + nombredocumento + extension;
+                  
                     usuario.Nombre = Vs_entrada["nombre"].ToString();
                     usuario.Apellido = Vs_entrada["apellido"].ToString();
                     usuario.Correo = Vs_entrada["correo"].ToString();
                     usuario.Contrasenia = Vs_entrada["contrasenia"].ToString();
                     usuario.Documento = Vs_entrada["documento"].ToString();
                     usuario.Telefono = Vs_entrada["telefono"].ToString();
-                    usuario.Hojavida = Vs_entrada["hojavida"].ToString();
+                    usuario.Hojavida = direccion;
                     usuario.Tipovehiculo = Vs_entrada["tipovehiculo"].ToString();
-                    usuario.Id_rol = int.Parse(Vs_entrada["id_rol"].ToString());
-                    usuario.Aprobacion = int.Parse(Vs_entrada["aprobacion"].ToString());
+                    usuario.Id_rol = 3;
+                    usuario.Aprobacion = 0;
                     usuario.Auditoria = Vs_entrada["nombre"].ToString();
-                    new Lregistar_domiciliario().LBTND_registrar1(usuario);
-                    return "registro exitoso";
+                  return  new Lregistar_domiciliario().GuardarHojaVida(hoja_vida,usuario,extension,direccion);
+                   
                 }
             }catch (Exception ex){
                 return "hay un problema interno: " + ex.StackTrace;
--- Logica/Lregistar_domiciliario.cs
+++ Logica/Lregistar_domiciliario.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using Data;
 using Utilitarios;
 
@@ -21,6 +23,49 @@ namespace Logica
             DAOUsuario dAOUsuario = new DAOUsuario();
             new DAOUsuario().insertUsuario(domiciliario);
         }
-     
+        public string GuardarHojaVida(byte[] archivo, UUsuario usuario, string extension, string direccion)
+        {
+            UMac datos = new UMac();
+
+            if (archivo != null)
+            {
+                extension = extension.ToLower();//Extension de la imagen y minusculas
+
+                if (extension == ".pdf" )
+                {
+                    try
+                    {
+                        //FileStream fileStream = new FileStream(direccion, FileMode.Create, FileAccess.ReadWrite);
+                        //fileStream.Write(Foto_producto, 0, Foto_producto.Length);
+                        //fileStream.Close();
+                        string direc = HttpContext.Current.Server.MapPath(direccion);
+                        FileStream fileStream = new FileStream(direc, FileMode.Create, FileAccess.ReadWrite);
+                        fileStream.Write(archivo, 0, archivo.Length);//mapea y guarda el archivo en la direccion
+                        fileStream.Close();
+
+                        new DAOUsuario().insertUsuario(usuario);
+                        
+                        datos.Url = "Aliado.aspx";
+                    }
+                    catch (Exception ex)
+                    {
+                        datos.Url = "No se pudo agregar producto    " + ex;
+                    }
+
+                }
+                else
+                {
+                    datos.Url = "Extension no valida, intentelo de nuevo";
+                }
+
+            }
+            else
+            {
+                datos.Url = "Foto No valida, intentelo de nuevo";
+            }
+
+            return datos.Url;
+
+        }//
     }
 }
